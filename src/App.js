1ed5e0b89033325a7e0463d3981743ba42000009
Bryan Valerin <b_valerin@hotmail.com>
2020-11-26 07:19:13 -0600

    restore iriginal jsx

--- src/App.js
+++ src/App.js
@@ -4,7 +4,20 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>I was chaged on feature branch</h1>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          I was changed on feature branch!
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }

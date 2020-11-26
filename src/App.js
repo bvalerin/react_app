cb2e5ef99fe020153cc5616fa7310cf17c78ba01
Bryan Valerin <b_valerin@hotmail.com>
2020-11-25 18:51:01 -0600

    Change on feature branch

--- src/App.js
+++ src/App.js
@@ -4,20 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         Hola Steven.! 
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>I was chaged on feature branch</h1>
     </div>
   );
 }
